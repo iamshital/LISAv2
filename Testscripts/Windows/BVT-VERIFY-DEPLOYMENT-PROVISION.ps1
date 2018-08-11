25e029ff7b2a6f979357454da17260ca36c38bd7
Shital Savekar <iamshital@users.noreply.github.com>
2018-08-12 01:20:03 +0530

    Changec reboot count to 3

--- Testscripts/Windows/BVT-VERIFY-DEPLOYMENT-PROVISION.ps1
+++ Testscripts/Windows/BVT-VERIFY-DEPLOYMENT-PROVISION.ps1
@@ -9,29 +9,30 @@ function Main {
 		$noIssues = CheckKernelLogs -allVMData $allVMData
 		if ($noIssues) {
 			$CurrentTestResult.TestSummary += CreateResultSummary -testResult "PASS" -metaData "FirstBoot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
-			$RestartStatus = RestartAllDeployments -allVMData $allVMData
-			if($RestartStatus -eq "True") {
-				$CurrentTestResult.TestSummary += CreateResultSummary -testResult "PASS" -metaData "Reboot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
-				LogMsg "Check 2: Checking call tracess again after Reboot > 30 seconds sleep"
-				Start-Sleep 30
-				$noIssues = CheckKernelLogs -allVMData $allVMData
-				if ($noIssues) {
-					$CurrentTestResult.TestSummary += CreateResultSummary -testResult "PASS" -metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
-					LogMsg "Test Result : PASS."
-					$testResult = "PASS"
+			for($i = 0; $i -lt 3; $i++){
+				$RestartStatus = RestartAllDeployments -allVMData $allVMData
+				if($RestartStatus -eq "True") {
+					$CurrentTestResult.TestSummary += CreateResultSummary -testResult "PASS" -metaData "Reboot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+					LogMsg "Check 2: Checking call tracess again after Reboot > 30 seconds sleep"
+					Start-Sleep 30
+					$noIssues = CheckKernelLogs -allVMData $allVMData
+					if ($noIssues) {
+						$CurrentTestResult.TestSummary += CreateResultSummary -testResult "PASS" -metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+						LogMsg "Test Result : PASS."
+						$testResult = "PASS"
+					}
+					else {
+						$CurrentTestResult.TestSummary += CreateResultSummary -testResult "FAIL" -metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+						LogMsg "Test Result : FAIL."
+						$testResult = "FAIL"
+					}
 				}
 				else {
-					$CurrentTestResult.TestSummary += CreateResultSummary -testResult "FAIL" -metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+					$CurrentTestResult.TestSummary += CreateResultSummary -testResult "FAIL" -metaData "Reboot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 					LogMsg "Test Result : FAIL."
 					$testResult = "FAIL"
 				}
 			}
-			else {
-				$CurrentTestResult.TestSummary += CreateResultSummary -testResult "FAIL" -metaData "Reboot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
-				LogMsg "Test Result : FAIL."
-				$testResult = "FAIL"
-			}
-
 		}
 		else {
 			$CurrentTestResult.TestSummary += CreateResultSummary -testResult "FAIL" -metaData "FirstBoot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
