74e558fa31d8d1a13c31f39ef7a5ac3f3bc1a82e
Shital Savekar <iamshital@users.noreply.github.com>
2018-08-15 21:00:11 +0530

    Changed fio jobs to 7

--- Testscripts/Linux/perf_fio.sh
+++ Testscripts/Linux/perf_fio.sh
@@ -214,7 +214,7 @@ RunFIO()
 			Thread=$startThread			
 			while [ $Thread -le $maxThread ]
 			do
-				numjobs=6
+				numjobs=7
 				iostatfilename="${IOSTATLOGDIR}/iostat-fio-${testmode}-${io}K-${Thread}td.txt"
 				nohup iostat -x 5 -t -y > $iostatfilename &
 				#capture blktrace output during test
